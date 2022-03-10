eb3b597122c6349683580d39703ad09c054d3e31
Mitch Eagles <mitch.eagles.stl@gmail.com>
2022-03-10 15:27:01 -0600

    morkserver start

---
+++ dev/mockserver.py
@@ -0,0 +1,61 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import re 
+import socket 
+from threading import Thread
+
+import requests
+from urllib.parse import urljoin
+from faker import Faker
+import responses
+
+fake = Faker()
+
+base_actionnetwork_API_URL = "https://actionnetwork.org/api/v2"
+
+@responses.activate
+def initiate_mock_actionnetwork(faker):
+	mock_get_user_endpoint = urljoin(base_actionnetwork_API_URL, "people")
+	mock_user_email = faker.email()
+	responses.add(responses.GET, mock_get_user_endpoint)
+
+
+def call_mock_api(URI, params=None):
+    return requests.get(
+        URI,
+        params=params,
+        headers={"OSDI-API-Token": settings.ACTIONNETWORK_API_KEYS["main"]},
+    ).json()
+
+class MockServerRequestHandler(BaseHTTPRequestHandler):	
+	USERS_PATTERN = re.compile(r'/users')
+
+	def do_GET(self):
+		if re.search(self.USERS_PATTERN, self.path):
+			# Add response status code
+			self.send_response(requests.code.ok)
+
+			# Add response headers
+			self.send_header('Content-Type', 'application/json; charset=utf-8')
+			self.end_headers()
+
+			# Add response content
+			response_content = json.dumps([])
+			self.wfile.write(response_content.encode('utf-8'))
+			return
+		elif re.search(self.EVENTS_PATTER, self.path):
+			# Response status code
+
+def get_free_port():
+	s = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+	s.bind(('localhost', 0))
+	address, port = s.getsockname()
+	s.close()
+	return port
+
+
+def start_mock_server(port):
+	mock_server = HTTPServer(('localhost', port), MockServerRequestHandler)
+	mock_server_thread = Thread(target=mock_server.serve_forever)
+	mock_server_thread.setDaemon(True)
+	mock_server_thread.start()
